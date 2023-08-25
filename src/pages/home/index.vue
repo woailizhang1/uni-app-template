b0d75f73834bf277c2e3862a5d6d79222fc9aa8f
AKe <3033244122@qq.com>
2023-08-25 10:51:13 +0800

    添加uni-ui

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,9 +1,6 @@
 <template>
-  <view class="w-32 py-2 rounded-md font-semibold text-white bg-pink-500 ring-4 ring-pink-300">
-    123</view
-  >
+  <view> </view>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
 </script>
